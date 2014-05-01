05fc8656fb5b9683b7dd4c6e2da0a77bf7c3cae9
elis <neng.elis20@gmail.com>
2014-05-01 07:40:08 +0700

    add

--- image.php
+++ image.php
@@ -2,4 +2,5 @@
       header('HTTP/1.0 301 Moved Permanently'); 
       header('Location: http://ws.assoc-amazon.com/widgets/q?_encoding=UTF8&Format=_SL_&ASIN='.$_GET["img"].'&MarketPlace=US&ID=AsinImage&WS=1&ServiceVersion=20070822');
 	  exit();
+	  
 ?>
